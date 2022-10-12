ec090c6386f8b87789407f8c7f484b3fc35e702e
gloxounet <matteo.glock@laposte.net>
2022-10-12 11:55:51 +0200

    docs

--- src/custom_time.py
+++ src/custom_time.py
@@ -2,6 +2,16 @@ from functools import wraps
 import time
 
 def timeit(verbose=True):
+    """
+    The function `timeit` returns a function `timeit_decorator` which returns a function
+    `timeit_wrapper` which returns the time taken to execute the function `func`
+    
+    Args:
+      verbose: If True, prints the time taken by the function. Defaults to True
+    
+    Returns:
+      A wrapper to time a function
+    """
     def timeit_decorator(func):
         @wraps(func)
         def timeit_wrapper(*args, **kwargs):
--- src/performance.py
+++ src/performance.py
@@ -10,16 +10,36 @@ urls = {
 
 @timeit(verbose=False)
 def getX(url,nb):
+    """
+    It creates a generator that will create nb requests to the url passed as argument
+    
+    Args:
+      url: the url to be requested
+      nb: number of requests
+    """
     rs = (grequests.get(url) for _ in range(nb))
     grequests.map(rs)
 
 
 def compareTimes(url):
+    """
+    It takes a url as input and returns a list of the execution times for the function getX for
+    different values of n
+    
+    Args:
+      url: the url of the website you want to test
+    
+    Returns:
+      A list of the execution times for each n in N.
+    """
     N = [x**3 for x in range(5,12)]
     execution_times = [getX(url,n) for n in N]
     return N,execution_times
   
 def main():
+    """
+    It gets the times for the 4 services and 9 services, and then plots them
+    """
     print("Getting times for 4 services being loadbalanced")
     N,T = compareTimes(urls['4services'])
     print("Done.")
