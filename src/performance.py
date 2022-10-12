87812823fcdc3b2b01978eba2eea6ca6b822a2df
gloxounet <matteo.glock@laposte.net>
2022-10-12 11:46:23 +0200

    fin

--- src/performance.py
+++ src/performance.py
@@ -4,28 +4,39 @@ import matplotlib.pyplot as plt
 from custom_time import timeit
 
 urls = {
-    'movie':'http://localhost:8000'
+    '4services':'http://localhost:8000',
+    '9services':'http://localhost:8001'
 }
 
-@timeit(verbose=True)
+@timeit(verbose=False)
 def getX(url,nb):
     rs = (grequests.get(url) for _ in range(nb))
     grequests.map(rs)
 
 
 def compareTimes(url):
-    N = [x**3 for x in range(10)]
+    N = [x**3 for x in range(5,12)]
     execution_times = [getX(url,n) for n in N]
     return N,execution_times
-
-def plotAndShow(X,Y):
-    plt.plot(X,Y)
-    plt.ylabel('ellapsed time')
-    plt.show()
-
+  
 def main():
-    N,T = compareTimes(urls['movie'])
-    plotAndShow(N,T)
+    print("Getting times for 4 services being loadbalanced")
+    N,T = compareTimes(urls['4services'])
+    print("Done.")
+
+    print("Getting times for 9 services being loadbalanced")
+    N2,T2 = compareTimes(urls['9services'])
+    print("Done.")
+    
+    # Plotting things
+    plt.plot(N,T,label="4")
+    plt.plot(N2,T2,label="9")
+
+    plt.ylabel('ellapsed time')
+    plt.yscale('linear')
+    plt.legend(loc='upper left')
+
+    plt.show()
 
 
 
